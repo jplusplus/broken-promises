98ae5d7d2d9a333868d2c78ce200b451b40a3ada
Edouard <edou4rd@gmail.com>
2013-11-05 09:44:55 +0100

    header fixed

--- Webapp/webapp.py
+++ Webapp/webapp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Encoding: utf-8
 # -----------------------------------------------------------------------------
-# Project : OKF - Spending Stories
+# Project : Broken Promises
 # -----------------------------------------------------------------------------
 # Author : Edouard Richard                                  <edou4rd@gmail.com>
 # -----------------------------------------------------------------------------
@@ -19,12 +19,12 @@ import os
 class CustomFlask(Eve):
 	jinja_options = Flask.jinja_options.copy()
 	jinja_options.update(dict(
-		block_start_string='[%',
-		block_end_string='%]',
-		variable_start_string='[[',
-		variable_end_string=']]',
-		comment_start_string='[#',
-		comment_end_string='#]',
+		block_start_string    = '[%',
+		block_end_string      = '%]',
+		variable_start_string = '[[',
+		variable_end_string   = ']]',
+		comment_start_string  = '[#',
+		comment_end_string    = '#]',
 	))
 
 app = CustomFlask(__name__, 
