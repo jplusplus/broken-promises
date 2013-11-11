8b4fc792c7fdd222d7329abc061730dd76c03dd8
Edouard <edou4rd@gmail.com>
2013-11-11 19:56:53 +0100

    licence

--- Sources/brokenpromises/utils.py
+++ Sources/brokenpromises/utils.py
@@ -5,11 +5,26 @@
 # -----------------------------------------------------------------------------
 # Author : Edouard Richard                                  <edou4rd@gmail.com>
 # -----------------------------------------------------------------------------
-# License : proprietary journalism++
+# License : GNU General Public License
 # -----------------------------------------------------------------------------
 # Creation : 28-Oct-2013
 # Last mod : 05-Nov-2013
 # -----------------------------------------------------------------------------
+# This file is part of Broken Promises.
+# 
+#     Broken Promises is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Broken Promises is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Broken Promises.  If not, see <http://www.gnu.org/licenses/>.
+
 import datetime
 
 def get_all_date_formats(year, month=None, day=None):
--- Sources/dateparser.py
+++ Sources/dateparser.py
@@ -25,6 +25,21 @@
 # Creation : 05-Nov-2013
 # Last mod : 08-Nov-2013
 # -----------------------------------------------------------------------------
+# This file is part of Broken Promises.
+# 
+#     Broken Promises is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Broken Promises is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Broken Promises.  If not, see <http://www.gnu.org/licenses/>.
+
 # From https://code.google.com/p/nltk/source/browse/trunk/nltk_contrib/nltk_contrib/timex.py
 
 import re
