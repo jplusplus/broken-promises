0af20740b2986786d2c76c201df9817275834c9c
Edouard <edou4rd@gmail.com>
2013-11-20 14:50:05 +0100

    scripts rq_worker added

---
+++ Scripts/rq_worker.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# Encoding: utf-8
+# -----------------------------------------------------------------------------
+# Project : Broken Promises
+# -----------------------------------------------------------------------------
+# Author : Edouard Richard                                  <edou4rd@gmail.com>
+# -----------------------------------------------------------------------------
+# License : GNU General Public License
+# -----------------------------------------------------------------------------
+# Creation : 20-Nov-2013
+# Last mod : 20-Nov-2013
+# -----------------------------------------------------------------------------
+# This file is part of Broken Promises.
+# 
+#     Broken Promises is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Broken Promises is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Broken Promises.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+conn   = redis.from_url(os.getenv('REDISTOGO_URL', 'redis://localhost:6379'))
+
+if __name__ == '__main__':
+	with Connection(conn):
+		worker = Worker(map(Queue, listen))
+		worker.work()
+
+# EOF
