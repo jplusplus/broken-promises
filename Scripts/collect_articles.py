3f48644af721541f1313caf984231b8b78ec5b63
Edouard <edou4rd@gmail.com>
2013-11-26 18:06:41 +0100

    collect_static updated

--- Scripts/collect_articles.py
+++ Scripts/collect_articles.py
@@ -38,16 +38,16 @@ reporter.REPORTER.register(reporter.StderrReporter())
 debug, trace, info, warning, error, fatal = reporter.bind("script_collect_articles")
 
 oparser = optparse.OptionParser(usage ="\n./%prog [options] year \n./%prog [options] year month\n./%prog [options] year month day")
-oparser.add_option("-C", "--nocache", action="store_true", dest="nocache",
-	help = "Prevents from using the cache", default=False)
+# oparser.add_option("-C", "--nocache", action="store_true", dest="nocache",
+# 	help = "Prevents from using the cache", default=False)
 oparser.add_option("-f", "--channelslistfile", action="store", dest="channels_file",
 	help = "Use this that as channels list to use", default=None)
 oparser.add_option("-c", "--channels", action="store", dest="channels_list",
 	help = "channels list comma separated", default=None)
-oparser.add_option("-m", "--mongodb", action="store", dest="mongodb_uri",
-	help = "uri to mongodb instance to persist results", default=None)
-oparser.add_option("-d", "--drop", action="store_true", dest="mongodb_drop",
-	help = "drop the previous articles from database before", default=False)
+# oparser.add_option("-m", "--mongodb", action="store", dest="mongodb_uri",
+# 	help = "uri to mongodb instance to persist results", default=None)
+# oparser.add_option("-d", "--drop", action="store_true", dest="mongodb_drop",
+# 	help = "drop the previous articles from database before", default=False)
 oparser.add_option("-o", "--output", action="store", dest="output_file",
 	help = "Specify  a file to write the export to. If you do not specify a file name, the program writes data to standard output (e.g. stdout)", default=None)
 
@@ -66,26 +66,7 @@ if options.channels_file:
 if options.channels_list:
 	channels = options.channels_list.split(",")
 
-results = CollectArticles(channels, *args).run()
-
-#  MONGO
-if options.mongodb_uri:
-	from pymongo import MongoClient
-	from urlparse import urlparse
-
-	client     = MongoClient(options.mongodb_uri)
-	db         = client[urlparse(options.mongodb_uri).path.split("/")[-1]]
-	collection = db['articles']
-
-	if options.mongodb_drop:
-		collection.remove()
-
-	for article in results:
-		previous = collection.find_one({"url" : article.url})
-		if not previous:
-			collection.insert(article.__dict__)
-		else:
-			collection.update({'_id':previous['_id']}, dict(previous.items() + article.__dict__.items()))
+results = CollectArticles(channels, *args, use_storage=True).run()
 
 # OUTPUT
 print dumps([_.__dict__ for _ in results]).encode('utf-8')
